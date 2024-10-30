176e97a103a21ee049e37e9e1c7b6b6bf06a0236
Rodrigo Pires <rec.pires7@gmail.com>
2024-10-30 00:50:49 -0300

    Add files via upload

---
+++ Exemplo 4.3.py
@@ -0,0 +1,10 @@
+A = int(input("Digite A: "))
+B = int(input("Digite B: "))
+if A == B:
+    print(f"Os dois números são iguais e é {A}")
+else:
+    if A < B:
+        print(f"O Menor numero é {A}")
+    else:
+        print(f"O Menor numero é {B}")
+print("Fim do programa")
